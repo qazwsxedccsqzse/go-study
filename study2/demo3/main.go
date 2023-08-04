fd3744837123229adb81825d222024e6acbfa695
neilwang <neil@onehumble.com>
2023-08-04 11:05:42 +0800

    feat: 新增讀 channel 範例

---
+++ study2/demo3/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	str := []byte("foobar")
+	ch := make(chan byte, len(str))
+	next := make(chan int)
+	wg := sync.WaitGroup{}
+	wg.Add(2)
+
+	for i := 0; i < len(str); i++ {
+		ch <- str[i]
+	}
+
+	next <- 1
+
+	close(ch)
+
+	go func() {
+		defer wg.Done()
+		for {
+			<-next
+			if v, ok := <-ch; ok {
+				fmt.Println("goroutine01", string(v))
+			} else {
+				// 會造成兩邊都關 channel
+				close(ch)
+				return
+			}
+			next <- 1
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+		for {
+			<-next
+			if v, ok := <-ch; ok {
+				fmt.Println("goroutine02", string(v))
+			} else {
+				// 會造成兩邊都關 channel
+				close(ch)
+				return
+			}
+			next <- 1
+		}
+	}()
+
+	wg.Wait()
+}
